1c6f0fe88744981cf626b501a5e712f24c58451f
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-12 01:29:56 +0530

    Note Addition Added

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom"
+import api from "../lib/axios";
+import { useEffect, useRef, useState } from "react";
+
+type Note = {
+    _id : string,
+    title : string,
+    content : string
+}
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+    const[notes, setNotes] = useState<Note[]>([]);
+    const[loading,setLoading] = useState(false);
+    const[error, setError] = useState<string | null>(null);
+    const titleRef = useRef<HTMLInputElement>(null)
+    const contentRef = useRef<HTMLInputElement>(null);
+
+    async function logout(){
+        await api.post('/auth/logout')
+        navigate('/login')
+    }
+
+    async function addNote(){
+        const title = titleRef.current?.value;
+        const content = contentRef.current?.value;
+        
+        if(!title || !content){
+            setError("Title and content are required")
+            return;
+        }
+
+        setError(null);
+        setLoading(true);
+
+        try{
+                const response = await api.post('/notes', {
+                title,
+                content
+            })
+            setNotes((prev)=>[...prev, response.data.note]);
+        }
+        catch(e){
+            setError("Failed to add Note");
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        const fetchNotes = async () =>{
+            setLoading(true);
+            try{
+                const response = await api.get('/notes');
+                setNotes(response.data.notes)
+            }catch(e){
+                setError("Error! Failed to Load Notes")
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchNotes();
+    },[])
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <button onClick={()=>logout()}>Logout</button>
+            {loading && <h1>Loading Notes...</h1>}
+            {error && <p>{error}</p>}
+            {!loading && notes.length===0 && <h1>No Notes! Add Some</h1>}
+            {notes.map((note) => (
+                <div key={note._id}>
+                    <h3>{note.title}</h3>
+                    <h3>{note.content}</h3>
+                </div>
+            ))}
+
+            <h1>Add Notes</h1>
+            <label>Add Title</label>
+            <input ref={titleRef} type="text" placeholder="Title"/>
+            <label>Add Content</label>
+            <input ref={contentRef} type="text" placeholder="Content"/>
+            <button onClick={()=>addNote()} >Add Note</button>
+        </div>
+    )
+}
+
+export default Dashboard
